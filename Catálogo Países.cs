5a10532a452ba5b0d800c403a37f6b35bc3096ac
Laura-Garbim <laura_garbim@hotmail.com>
2024-09-09 16:36:27 -0300

    Update Catálogo Países.cs

--- Catálogo Países.cs
+++ Catálogo Países.cs
@@ -9,22 +9,39 @@ class Program
         var countryInfo = new Dictionary<string, CountryInfo>
         {
             {"Brasil", new CountryInfo("Brasília", "América do Sul", new List<string> {"Argentina", "Bolívia", "Colômbia", "Guiana", "Paraguai", "Peru", "Suriname", "Uruguai", "Venezuela"}, "Português", "Oceano Atlântico")},
+           
             {"Estados Unidos", new CountryInfo("Washington, D.C.", "América do Norte", new List<string> {"Canadá", "México"}, "Inglês", "Oceano Atlântico e Oceano Pacífico")},
+            
             {"França", new CountryInfo("Paris", "Europa", new List<string> {"Bélgica", "Luxemburgo", "Alemanha", "Suíça", "Itália", "Espanha"}, "Francês", "Oceano Atlântico e Mar Mediterrâneo")},
+            
             {"Alemanha", new CountryInfo("Berlim", "Europa", new List<string> {"Dinamarca", "Polônia", "República Tcheca", "Áustria", "Suíça", "França", "Luxemburgo", "Bélgica", "Países Baixos"}, "Alemão", "Nenhum")},
+            
             {"China", new CountryInfo("Pequim", "Ásia", new List<string> {"Rússia", "Mongólia", "Afeganistão", "Paquistão", "Índia", "Nepal", "Butão", "Birmania", "Laos", "Vietnã", "Hong Kong", "Macau"}, "Chinês", "Oceano Pacífico")},
+            
             {"Japão", new CountryInfo("Tóquio", "Ásia", new List<string> {}, "Japonês", "Oceano Pacífico")},
+            
             {"Argentina", new CountryInfo("Buenos Aires", "América do Sul", new List<string> {"Chile", "Bolívia", "Paraguai", "Brasil", "Uruguai"}, "Espanhol", "Oceano Atlântico")},
+            
             {"Índia", new CountryInfo("Nova Délhi", "Ásia", new List<string> {"Paquistão", "China", "Nepal", "Butão", "Bangladesh", "Myanmar"}, "Hindi, Inglês", "Oceano Índico")},
+            
             {"Canadá", new CountryInfo("Ottawa", "América do Norte", new List<string> {"Estados Unidos"}, "Inglês, Francês", "Oceano Atlântico e Oceano Pacífico")},
+            
             {"México", new CountryInfo("Cidade do México", "América do Norte", new List<string> {"Estados Unidos", "Guatemala", "Belize"}, "Espanhol", "Oceano Pacífico e Oceano Atlântico")},
+            
             {"Itália", new CountryInfo("Roma", "Europa", new List<string> {"França", "Suíça", "Áustria", "Eslovênia"}, "Italiano", "Mar Mediterrâneo")},
+            
             {"Espanha", new CountryInfo("Madri", "Europa", new List<string> {"França", "Andorra", "Portugal"}, "Espanhol", "Oceano Atlântico e Mar Mediterrâneo")},
+            
             {"Egito", new CountryInfo("Cairo", "África", new List<string> {"Líbia", "Sudão", "Palestina", "Israel"}, "Árabe", "Mar Mediterrâneo e Mar Vermelho")},
+            
             {"Austrália", new CountryInfo("Canberra", "Oceania", new List<string> {}, "Inglês", "Oceano Pacífico e Oceano Índico")},
+            
             {"Turquia", new CountryInfo("Ancara", "Ásia", new List<string> {"Grécia", "Bulgária", "Geórgia", "Armênia", "Azerbaijão", "Irã", "Iraque", "Síria"}, "Turco", "Mar Mediterrâneo, Mar Egeu e Mar Negro")},
+            
             {"África do Sul", new CountryInfo("Pretória", "África", new List<string> {"Namíbia", "Botswana", "Zimbábue", "Moçambique", "Eswatini", "Lesoto"}, "Africâner, Inglês", "Oceano Atlântico e Oceano Índico")},
+            
             {"Noruega", new CountryInfo("Oslo", "Europa", new List<string> {"Suécia", "Finlândia", "Rússia"}, "Norueguês", "Oceano Atlântico e Mar de Barents")},
+            
             {"Suécia", new CountryInfo("Estocolmo", "Europa", new List<string> {"Noruega", "Finlândia"}, "Sueco", "Oceano Atlântico e Mar Báltico")}
         };
 
